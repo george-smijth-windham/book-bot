c8a82fd4a6acec09cd9ce58d63847ffe53ab636b
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-20 11:18:11 +0000

    update main to print book to console

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+from pathlib import Path as p
+
+root_dir = p.cwd()
+book_dir = p.joinpath(root_dir, "books")
+book_file_path = p.joinpath(book_dir, "frankenstein.txt")
+
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+def main():
+    file_contents = get_book_text(book_file_path)
+    print(file_contents)
+
+
+main()

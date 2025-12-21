f600c5fc39c38702062f23f250dffbdec9a9f8ab
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-21 11:57:04 +0000

    add fn to get book word count

--- src/main.py
+++ src/main.py
@@ -10,9 +10,14 @@ def get_book_text(file_path):
         return f.read()
 
 
+def get_num_words(file_contents):
+    return len(file_contents.split())
+
+
 def main():
     file_contents = get_book_text(book_file_path)
-    print(file_contents)
+    num_words = get_num_words(file_contents)
+    print(f"Found {num_words} total words")
 
 
 main()

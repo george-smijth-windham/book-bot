69983955922025a0d2008f710bbbb0a0a855d0ca
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-22 09:38:59 +0000

    update main to import stats

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 from pathlib import Path as p
+from stats import get_book_text, get_num_words
 
 root_dir = p.cwd()
 book_dir = p.joinpath(root_dir, "books")
 book_file_path = p.joinpath(book_dir, "frankenstein.txt")
-
-
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read()
-
-
-def get_num_words(file_contents):
-    return len(file_contents.split())
 
 
 def main():

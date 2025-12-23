54d5b84a00bebc59fd5b074c84eeb0d12bb055c6
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-23 14:35:06 +0000

    add fn to get char count in file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path as p
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_char_counts
 
 root_dir = p.cwd()
 book_dir = p.joinpath(root_dir, "books")
@@ -10,6 +10,8 @@ def main():
     file_contents = get_book_text(book_file_path)
     num_words = get_num_words(file_contents)
     print(f"Found {num_words} total words")
+    char_counts = get_char_counts(file_contents)
+    print(char_counts)
 
 
 main()
--- src/stats.py
+++ src/stats.py
@@ -5,3 +5,14 @@ def get_book_text(file_path):
 
 def get_num_words(file_contents):
     return len(file_contents.split())
+
+
+def get_char_counts(file_contents):
+    char_counts = {}
+
+    for char in file_contents:
+        c = char.lower()
+        if c not in char_counts:
+            char_counts[c] = 0
+        char_counts[c] += 1
+    return char_counts

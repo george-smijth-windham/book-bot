30de5d5278bc7e0f8aedf4f29edc4c55093fc840
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-24 14:13:56 +0000

    add fn to generate book report

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
 from pathlib import Path as p
-from stats import get_book_text, get_num_words, get_char_counts
+from stats import (
+    get_book_text,
+    get_num_words,
+    get_char_counts,
+    get_sorted_char_counts,
+    get_report,
+)
 
-root_dir = p.cwd()
-book_dir = p.joinpath(root_dir, "books")
-book_file_path = p.joinpath(book_dir, "frankenstein.txt")
+ROOT_DIR = p.cwd()
+BOOK_DIR = p.joinpath(ROOT_DIR, "books")
+BOOK_FILE_PATH = p.joinpath(BOOK_DIR, "frankenstein.txt")
 
 
 def main():
-    file_contents = get_book_text(book_file_path)
+    file_contents = get_book_text(BOOK_FILE_PATH)
     num_words = get_num_words(file_contents)
-    print(f"Found {num_words} total words")
     char_counts = get_char_counts(file_contents)
-    print(char_counts)
+    report = get_report(num_words, char_counts)
+    print(report)
 
 
 main()
--- src/stats.py
+++ src/stats.py
@@ -16,3 +16,41 @@ def get_char_counts(file_contents):
             char_counts[c] = 0
         char_counts[c] += 1
     return char_counts
+
+
+def sort_on(char):
+    return char["num"]
+
+
+def get_sorted_char_counts(char_counts):
+    chars = []
+    for c, count in char_counts.items():
+        if c.isalpha():
+            chars.append({"char": c, "num": count})
+    chars.sort(reverse=True, key=sort_on)
+    return chars
+
+
+def get_char_count_string(sorted_char_counts):
+    char_count_str = ""
+    for i in range(len(sorted_char_counts)):
+        c, count = sorted_char_counts[i].values()
+        char_count_str = (
+            f"{c}: {count}" if not char_count_str else f"{char_count_str}\n{c}: {count}"
+        )
+    return char_count_str
+
+
+def get_report(*args):
+    num_words, char_counts = args
+    output = ""
+    sorted_char_counts = get_sorted_char_counts(char_counts)
+    char_count_string = get_char_count_string(sorted_char_counts)
+    output = output + "============ BOOKBOT ============"
+    output = output + f"\nAnalyzing book found at books/frankenstein.txt..."
+    output = output + f"\n----------- Word Count ----------"
+    output = output + f"\nFound {num_words} total words"
+    output = output + f"\n--------- Character Count -------"
+    output = output + f"\n{char_count_string}"
+    output = output + f"\n============= END ==============="
+    return output

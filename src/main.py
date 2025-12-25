6dc20137853bceadb54f4d026b4deb7dcf83f01f
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2025-12-25 13:39:32 +0000

    update main to accept command line arguments

--- src/main.py
+++ src/main.py
@@ -3,21 +3,25 @@ from stats import (
     get_book_text,
     get_num_words,
     get_char_counts,
-    get_sorted_char_counts,
     get_report,
 )
+import sys
+
 
 ROOT_DIR = p.cwd()
-BOOK_DIR = p.joinpath(ROOT_DIR, "books")
-BOOK_FILE_PATH = p.joinpath(BOOK_DIR, "frankenstein.txt")
 
 
 def main():
-    file_contents = get_book_text(BOOK_FILE_PATH)
-    num_words = get_num_words(file_contents)
-    char_counts = get_char_counts(file_contents)
-    report = get_report(num_words, char_counts)
-    print(report)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    _, book_name = sys.argv
+    book_file_path = p.joinpath(ROOT_DIR, book_name)
+    book_contents = get_book_text(book_file_path)
+    book_num_words = get_num_words(book_contents)
+    book_char_counts = get_char_counts(book_contents)
+    book_report = get_report(book_num_words, book_char_counts, book_name)
+    print(book_report)
 
 
 main()
